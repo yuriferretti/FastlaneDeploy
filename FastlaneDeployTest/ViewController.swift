cd588db5ccb1731901bb2670ae19ecb4405a5a13
Yuri Ferretti <yuri.ferretti@luizalabs.com>
2015-12-21 15:03:23 -0200

    first commit

---
+++ FastlaneDeployTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FastlaneDeployTest
+//
+//  Created by Yuri Ferretti on 12/21/15.
+//  Copyright (c) 2015 Yuri Ferretti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
